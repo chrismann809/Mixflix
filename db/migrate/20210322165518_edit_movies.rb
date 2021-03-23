24b0100ab99f4a55b72a6e9efc935f5e8ff126d0
chrismann809 <chrismann809@gmail.com>
2021-03-22 23:23:39 -0400

    bugfixes

--- db/migrate/20210322165518_edit_movies.rb
+++ db/migrate/20210322165518_edit_movies.rb
@@ -1,6 +1,6 @@
 class EditMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column :movies, :year, :integer, null: false
-    add_column :movies, :content_rating, :string, null: false
+    add_column :movies, :year, :integer
+    add_column :movies, :content_rating, :string
   end
 end
--- db/migrate/20210323031806_edit_movies3.rb
+++
@@ -1,7 +0,0 @@
-class EditMovies3 < ActiveRecord::Migration[5.2]
-  def change
-    add_column :movies, :year, :integer
-    add_column :movies, :content_rating, :string
-    add_column :movies, :video_length, :integer
-  end
-end
