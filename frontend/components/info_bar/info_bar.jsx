b1622b60eb76303e10b67417082b6d6f78d73891
chrismann809 <chrismann809@gmail.com>
2021-03-26 10:57:05 -0400

    added footer links

--- frontend/components/info_bar/info_bar.jsx
+++ frontend/components/info_bar/info_bar.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { SiGithub, SiLinkedin } from 'react-icons/si'
+import { IconContext } from 'react-icons'
 
 
 const InfoBar = ({currentUser, currentPage, logout, processForm}) => {
   const landingInfo = () => (
     <div className= "landing-login-info-container">
+      <a href="http://github.com/chrismann809"><IconContext.Provider value={{ color: "white" }}><SiGithub className="footer-git"/></IconContext.Provider></a>
+      <a href="http://linkedin.com/"><IconContext.Provider value={{ color: "white" }}><SiLinkedin className="footer-linked"/></IconContext.Provider></a>
     </div>
   );
 
   const loginInfo = () => (
     <div className= "landing-login-info-container">
+      <a href="http://linkedin.com/"><IconContext.Provider value={{ color: "white" }}><SiLinkedin className="footer-linked"/></IconContext.Provider></a>
+      <a href="http://github.com/chrismann809"><IconContext.Provider value={{ color: "white" }}><SiGithub className="footer-git"/></IconContext.Provider></a>
     </div>
   )
   
   const signupInfo = () => (
     <div className= "signup-info-container">
+      <a href="http://github.com/chrismann809"><IconContext.Provider value={{ color: "black" }}><SiGithub className="footer-git"/></IconContext.Provider></a>
+      <a href="http://linkedin.com/"><IconContext.Provider value={{ color: "black" }}><SiLinkedin className="footer-linked"/></IconContext.Provider></a>
     </div>
   )
 
