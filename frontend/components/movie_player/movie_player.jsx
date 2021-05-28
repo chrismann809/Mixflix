41e7cc5b74a1a6b6588812af23472f3882e56b03
chrismann809 <chrismann809@gmail.com>
2021-05-28 12:21:41 -0400

    updated watch screen

--- frontend/components/movie_player/movie_player.jsx
+++ frontend/components/movie_player/movie_player.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { BsFillPlayFill, BsFillPauseFill, BsFillVolumeUpFill, BsFillVolumeMuteFill, BsFullscreen, BsFullscreenExit } from "react-icons/bs";
+import ProgressBar from "./progress_bar"
 
 export default class MoviePlayer extends React.Component {
     constructor(props) {
@@ -11,7 +12,8 @@ export default class MoviePlayer extends React.Component {
             description: description,
             videoUrl: videoUrl,
             paused: false,
-            muted: false
+            muted: false,
+            fullscreen: false,
         }
 
         this.video;
@@ -22,6 +24,7 @@ export default class MoviePlayer extends React.Component {
 
     componentDidMount() {
         this.video = document.getElementById("video");
+        this.element = document.documentElement;
     }
 
     toggleMute() {
@@ -38,13 +41,45 @@ export default class MoviePlayer extends React.Component {
         })
     }
 
+    toggleFullscreen() {
+        const elem = document.documentElement;
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+          } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+          } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
+
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        }
+
+        this.setState({
+            fullscreen: !this.state.fullscreen
+        })
+    }
+
     render() {
         // debugger
         return (
             <div className="video-container">
                 { this.state.muted ?  <video id="video" src={this.props.location.state.videoUrl} autoPlay muted onClick={this.togglePlay} /> : <video id="video" src={this.props.location.state.videoUrl} autoPlay onClick={this.togglePlay} /> }
                 {/* <video id="video" src={this.props.location.state.videoUrl} autoPlay/> */}
+                <div>
+                    <ProgressBar />
+                </div>
+                <div className="video-controls">
+                    { this.state.paused ? <BsFillPlayFill className="video-play" onClick={this.togglePlay} /> : <BsFillPauseFill className="video-play" onClick={this.togglePlay} /> }
+                    { this.state.muted ? <BsFillVolumeMuteFill className="video-volume" onClick={this.toggleMute} /> : <BsFillVolumeUpFill className="video-volume" onClick={this.toggleMute} /> }
+                    { this.state.fullscreen ? <BsFullscreenExit className="video-fullscreen" onClick={this.toggleFullscreen} /> : <BsFullscreen className="video-fullscreen" onClick={this.toggleFullscreen} /> }
+                </div>
             </div>
+            
         )
     }
 }
